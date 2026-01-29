cf53528049642caee9d1af76c1bcc45ae7cdd0f8
Oleksandr <aleks6699@gmail.com>
2026-01-29 22:57:30 +0200

    build: docker

--- tandem/back-end/src/prisma.service.ts
+++ tandem/back-end/src/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client.js';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
