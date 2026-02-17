fc2359248f99beeb19abbfb2b8ffe2fe919354a4
nasteew <anastasiya.w53@gmail.com>
2026-02-17 16:44:33 +0300

    feat: create users service

--- tandem/back-end/src/user/users.service.ts
+++ tandem/back-end/src/user/users.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service.js';
-import bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma.service.js';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(email: string, password: string) {
-    const hash = bcrypt.hashSync(password, 10);
-    return this.prisma.user.create({
-      data: { email, password: hash },
-    });
+  async createUser(data: { email: string; password: string; name: string }) {
+    return this.prisma.user.create({ data });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async updateUser(
+    id: number,
+    data: Partial<{ email: string; password: string; name: string }>,
+  ) {
+    return this.prisma.user.update({ where: { id }, data });
   }
 }
