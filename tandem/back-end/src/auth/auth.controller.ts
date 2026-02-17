1108de5771434ce181e960cbd73b6cb23506a215
nasteew <anastasiya.w53@gmail.com>
2026-02-17 21:59:08 +0300

    fix: fix main pipes and add public

--- tandem/back-end/src/auth/auth.controller.ts
+++ tandem/back-end/src/auth/auth.controller.ts
@@ -9,18 +9,21 @@ import {
 import { AuthService } from './auth.service.js';
 import { LoginDto } from './dto/login.dto.js';
 import { RegisterDto } from './dto/register.dto.js';
+import { Public } from './decorators/public.decorator.js';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto.email, loginDto.password);
   }
 
   @HttpCode(HttpStatus.CREATED)
+  @Public()
   @Post('register')
   signIn(@Body() registerDto: RegisterDto) {
     return this.authService.register(
