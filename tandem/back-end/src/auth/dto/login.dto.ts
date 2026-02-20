c753bf5c7c0731047c02b43f5a125d70ca4cf43d
nasteew <anastasiya.w53@gmail.com>
2026-02-20 20:21:00 +0300

    feat: add swagger to dto

--- tandem/back-end/src/auth/dto/login.dto.ts
+++ tandem/back-end/src/auth/dto/login.dto.ts
@@ -1,6 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail() email: string;
-  @IsNotEmpty() password: string;
+  @ApiProperty({
+    description: 'The email of the user for login',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'strongPassword123',
+  })
+  @IsNotEmpty()
+  password: string;
 }
--- tandem/back-end/src/auth/dto/register.dto.ts
+++ tandem/back-end/src/auth/dto/register.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({
+    description: 'The email of the user for registration',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user (minimum 6 characters)',
+    example: 'strongPassword123',
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
   @IsNotEmpty()
   name: string;
 }
