46c15df2932cf2348b8c2779df2919488585ceec
nasteew <anastasiya.w53@gmail.com>
2026-02-20 18:31:32 +0300

    fix: fix jwt strategy, add jwt payload

--- tandem/back-end/src/auth/strategy/jwt.strategy.ts
+++ tandem/back-end/src/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import type { JwtPayload } from '../interfaces/jwt-payload.interface.js';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,11 +14,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: any) {
+  validate(payload: JwtPayload) {
     return {
       userId: payload.sub,
-      email: payload.email,
-      name: payload.name,
     };
   }
 }
