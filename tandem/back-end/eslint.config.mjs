dbbe2af99c172711e5b96dbc09d59580be2d323f
nasteew <anastasiya.w53@gmail.com>
2026-02-22 18:35:50 +0300

    feat: create an authorization service
    * docs: add two notes
    
    * feat: add prisma schema
    
    * fix: fix prisma schema
    
    * feat: add user service
    
    * feat: add user module
    
    * fix: fix prisma schema
    
    * feat: create users service
    
    * feat: create users module
    
    * feat: add auth service
    
    * feat: add auth controller and module
    
    * feat:  add app structure
    
    * feat: add dto
    
    * fix: fix main
    
    * fix: fix config
    
    * feat:  add new prisma migration
    
    * feat: add jwt
    
    * docs: add new note
    
    * fix: fix main pipes and add public
    
    * fix: add global guard
    
    * fix: fix auth config
    
    * feat: delete development-notes
    
    * fix: fix users service
    
    * feat: add refresh function
    
    * feat: add refresh
    
    * fix: fix salt round
    
    * fix: fix jwt strategy, add jwt payload
    
    * feat: add set cookie method
    
    * fix: return access token
    
    * feat: add logout
    
    * fix: fix logout
    
    * fix: fix refresh method
    
    * feat: add swagger to auth controller
    
    * feat: add swagger to dto
    
    * fix: fix swagger
    
    * fix: fix config
    
    * fix: fix config
    
    * fix: update functions and values
    
    * feat: add refresh token created time
    
    * fix: update refresh function

--- tandem/back-end/eslint.config.mjs
+++ tandem/back-end/eslint.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -26,10 +25,8 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
--- tandem/back-end/src/main.ts
+++ tandem/back-end/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +15,15 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+await bootstrap();
