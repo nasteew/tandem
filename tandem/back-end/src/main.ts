1d4da39403af3e3fc22fd2bfef06112e7be53210
anastasiashlyk <161599870+anastasiashlyk@users.noreply.github.com>
2026-02-24 18:10:14 +0100

    feat: create ai service
    * feat: add ai module
    
    * feat: add ai controller
    
    * feat: add ai service
    
    * feat: connect AI module to app
    
    * fix: some fixes
    
    * docs: add diary note
    
    * refactor: add note
    
    * fix: add open router key
    
    * fix: move logic to service
    
    * fix: improve agentPage
    
    * fix: add chat dto for security
    
    * fix: adjust typing speed & send msg by clicking enter
    
    * fix: some fixes
    
    * refactor: delete development notes
    
    * feat: add input component
    
    * fix: allow public usage of ai agent
    
    * fix: fix location
    
    * fix: explicit any
    
    * fix: use effect
    
    * fix: use effect
    
    * fix: unnecessary styles
    
    * feat: add message template

--- tandem/back-end/src/main.ts
+++ tandem/back-end/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -23,6 +24,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3001);
 }
