40d3a80478b8f39b33d84d1b1b64a3d53ccf4cd1
nasteew <anastasiya.w53@gmail.com>
2026-02-27 23:04:45 +0300

    fix: fix profile stats

--- tandem/front-end/src/pages/ProfilePage/components/ProfileStatItem.tsx
+++ tandem/front-end/src/pages/ProfilePage/components/ProfileStatItem.tsx
@@ -8,7 +8,7 @@ interface ProfileStatItemProps {
 export const ProfileStatItem = ({ label, value }: ProfileStatItemProps) => {
   return (
     <article className="p-3 max-w-36 rounded-xl bg-white/5 border border-[var(--color-border-light)] backdrop-blur-md flex-1 text-center">
-      <p className="_title_otbu1_89 text-base uppercase font-bold">{label}</p>
+      <p className="text-[var(--color-text-muted)] text-base uppercase font-bold">{label}</p>
       <p className="text-white font-bold text-lg">
         <AnimatedNumber value={value} />
       </p>
