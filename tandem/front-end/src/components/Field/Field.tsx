ace6dc1cac5162cfba87bb829022ff8b33668fbd
nasteew <anastasiya.w53@gmail.com>
2026-03-01 16:35:22 +0300

    feat: add field

--- tandem/front-end/src/components/Field/Field.tsx
+++ tandem/front-end/src/components/Field/Field.tsx
@@ -1,14 +1,13 @@
 import { Button } from '../ui/Button/Button';
-import { Input } from '../ui/Input/Input';
 
 interface FieldProps {
   label: string;
-  value: string;
+  value?: string;
   editing: boolean;
   textarea?: boolean;
-  onEdit: () => void;
-  onCancel: () => void;
-  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onEdit?: () => void;
+  onCancel?: () => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
 export const Field = ({
@@ -26,48 +25,50 @@ export const Field = ({
     <div className="space-y-1 group">
       <div className="flex justify-between items-center">
         <span className="text-sm text-[var(--color-text-muted)] uppercase font-bold">{label}</span>
+
         {!editing ? (
-          <Button
-            size="sm"
-            variant="ghost"
-            className="transition-shadow duration-300 hover:shadow-[0_0_10px_rgb(96,165,250)] hover:text-white"
-            onClick={onEdit}
-          >
+          <Button size="sm" variant="ghost" onClick={onEdit}>
             Edit
           </Button>
         ) : (
-          <Button
-            size="sm"
-            variant="ghost"
-            className="transition-shadow duration-300 hover:shadow-[0_0_10px_rgb(96,165,250)] hover:text-white"
-            onClick={onCancel}
-          >
+          <Button size="sm" variant="ghost" onClick={onCancel}>
             Cancel
           </Button>
         )}
       </div>
 
-      {!editing ? (
-        <div
-          className={`rounded-lg bg-white/5 border border-[var(--color-border-light)] transition ${fieldHeight} px-4 py-2`}
-        >
+      <div
+        className={`
+          rounded-lg ${editing ? 'bg-slate-950 border border-slate-800' : 'bg-white/5 border-[var(--color-border-light)]'} border border-[var(--color-border-light)]
+          transition px-4 py-2 ${fieldHeight} flex items-center
+        `}
+      >
+        {!editing ? (
           <p className="text-white text-sm truncate">{value}</p>
-        </div>
-      ) : textarea ? (
-        <textarea
-          value={value}
-          onChange={onChange}
-          rows={1}
-          className={`w-full rounded-lg bg-white/5 border border-[var(--color-border-light)] text-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 transition resize-none ${fieldHeight} px-4 py-2 align-top`}
-        />
-      ) : (
-        <Input
-          value={value}
-          onChange={onChange}
-          className={`text-sm transition ${fieldHeight} px-4 py-2.5`}
-          maxLength={50}
-        />
-      )}
+        ) : textarea ? (
+          <textarea
+            value={value}
+            onChange={onChange}
+            rows={1}
+            className="
+              w-full text-sm text-white bg-transparent
+              outline-none border-none resize-none
+              leading-none
+            "
+          />
+        ) : (
+          <input
+            value={value}
+            onChange={onChange}
+            className="
+              w-full text-sm text-white bg-transparent
+              outline-none border-none
+              leading-none
+            "
+            maxLength={50}
+          />
+        )}
+      </div>
     </div>
   );
 };
