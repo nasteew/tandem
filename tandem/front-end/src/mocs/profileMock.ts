7f0b44b3c1879784b784ffa16690f4293e342932
nasteew <anastasiya.w53@gmail.com>
2026-03-04 23:57:42 +0300

    fix: fix mock data

--- tandem/front-end/src/mocs/profileMock.ts
+++ tandem/front-end/src/mocs/profileMock.ts
@@ -1,6 +1,7 @@
 import type { UserProfile } from '@/types/UserProfile';
 
 export const profileMock: UserProfile = {
+  id: 1,
   name: 'Test User',
   email: 'test@example.com',
   about: 'Lorem ipsum dolor sit amet.',
