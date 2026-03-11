d08be1bbf75145a95e00883d2f262c2e33f046a1
nasteew <anastasiya.w53@gmail.com>
2026-03-11 15:18:56 +0300

    feat: set up test config

---
+++ tandem/front-end/src/setup.test.tsx
@@ -0,0 +1,11 @@
+// setup.test.tsx
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+function Hello() {
+  return <div>Hello World</div>;
+}
+
+test('renders hello world text', () => {
+  render(<Hello />);
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
+});
