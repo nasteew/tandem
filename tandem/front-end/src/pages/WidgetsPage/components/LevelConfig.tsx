ebfb4a05ea95954812b43e5829192380474fdd37
nasteew <anastasiya.w53@gmail.com>
2026-03-27 20:02:38 +0300

    fix: add adaptive

--- tandem/front-end/src/pages/WidgetsPage/components/LevelConfig.tsx
+++ tandem/front-end/src/pages/WidgetsPage/components/LevelConfig.tsx
@@ -44,7 +44,7 @@ export const LevelConfig = ({
         Configure Level
       </h3>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <DifficultySelector
           difficulties={difficulties}
           selected={difficulty}
