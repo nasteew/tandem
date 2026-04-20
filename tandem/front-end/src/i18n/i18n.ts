41e2fc55f909b3092d7a4f67a5409af54be81e0d
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-04-20 19:42:13 +0200

    fix: add supported languages

--- tandem/front-end/src/i18n/i18n.ts
+++ tandem/front-end/src/i18n/i18n.ts
@@ -1,40 +1,71 @@
-import i18n from "i18next"
-import { initReactI18next } from "react-i18next"
-import LanguageDetector from "i18next-browser-languagedetector"
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enNavbar from "./locales/en/navbar.json"
-import ruNavbar from "./locales/ru/navbar.json"
-import enHero from "./locales/en/hero.json"
-import ruHero from "./locales/ru/hero.json"
-import enFeatures from "./locales/en/features.json"
-import ruFeatures from "./locales/ru/features.json"
-import enDashboard from "./locales/en/dashboard.json"
-import ruDashboard from "./locales/ru/dashboard.json"
-import enStatistic from "./locales/en/statistic.json"
-import ruStatistic from "./locales/ru/statistic.json"
-import enAuth from "./locales/en/auth.json"
-import ruAuth from "./locales/ru/auth.json"
-import enAgent from "./locales/en/agent.json"
-import ruAgent from "./locales/ru/agent.json"
-import enWidgets from "./locales/en/widgets.json"
-import ruWidgets from "./locales/ru/widgets.json"
-import enProfile from "./locales/en/profile.json"
-import ruProfile from "./locales/ru/profile.json"
+import enNavbar from './locales/en/navbar.json';
+import ruNavbar from './locales/ru/navbar.json';
+import enHero from './locales/en/hero.json';
+import ruHero from './locales/ru/hero.json';
+import enFeatures from './locales/en/features.json';
+import ruFeatures from './locales/ru/features.json';
+import enDashboard from './locales/en/dashboard.json';
+import ruDashboard from './locales/ru/dashboard.json';
+import enStatistic from './locales/en/statistic.json';
+import ruStatistic from './locales/ru/statistic.json';
+import enAuth from './locales/en/auth.json';
+import ruAuth from './locales/ru/auth.json';
+import enAgent from './locales/en/agent.json';
+import ruAgent from './locales/ru/agent.json';
+import enWidgets from './locales/en/widgets.json';
+import ruWidgets from './locales/ru/widgets.json';
+import enProfile from './locales/en/profile.json';
+import ruProfile from './locales/ru/profile.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { navbar: enNavbar, hero: enHero, features: enFeatures, dashboard: enDashboard, statistic: enStatistic, auth: enAuth, agent: enAgent, widgets: enWidgets, profile: enProfile },
-      ru: { navbar: ruNavbar, hero: ruHero, features: ruFeatures, dashboard: ruDashboard, statistic: ruStatistic, auth: ruAuth, agent: ruAgent, widgets: ruWidgets, profile: ruProfile }
+      en: {
+        navbar: enNavbar,
+        hero: enHero,
+        features: enFeatures,
+        dashboard: enDashboard,
+        statistic: enStatistic,
+        auth: enAuth,
+        agent: enAgent,
+        widgets: enWidgets,
+        profile: enProfile,
+      },
+      ru: {
+        navbar: ruNavbar,
+        hero: ruHero,
+        features: ruFeatures,
+        dashboard: ruDashboard,
+        statistic: ruStatistic,
+        auth: ruAuth,
+        agent: ruAgent,
+        widgets: ruWidgets,
+        profile: ruProfile,
+      },
     },
-    fallbackLng: "en",
-    ns: ['navbar', 'hero', 'features', 'dashboard', 'statistic', 'auth', 'agent', 'widgets', 'profile'],
+    supportedLngs: ['en', 'ru'],
+    fallbackLng: 'en',
+    ns: [
+      'navbar',
+      'hero',
+      'features',
+      'dashboard',
+      'statistic',
+      'auth',
+      'agent',
+      'widgets',
+      'profile',
+    ],
     defaultNS: 'navbar',
     interpolation: {
-      escapeValue: false
-    }
-  })
+      escapeValue: false,
+    },
+  });
 
-export default i18n
+export default i18n;
